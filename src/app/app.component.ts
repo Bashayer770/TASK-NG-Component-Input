0c1d4afb9f9736a14a6cfef740c6e098dd1aef68
Bashayer770 <Beshow95@gmail.com>
2025-04-06 12:03:21 +0300

    Bashayer and zainab Alhallaq

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { pets } from '../data/pets';
+import { CatComponent } from './cat/cat.component';
+import { PetComponent } from './pet/pet.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [PetComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'TASK-NG-Passing-Data';
   pets = pets;
 }
